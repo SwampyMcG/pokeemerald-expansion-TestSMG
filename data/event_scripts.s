de73c144917dd38eb3039cb277e104b2f5a20146
SwampyMcG <adammcg123@gmail.com>
2026-07-26 19:26:23 -0400

    Map Changes

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1738,3 +1738,9 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
 	.include "data/scripts/wild_encounter.inc"
+
+	.include "data/maps/AllstarVillage/scripts.inc"
+
+	.include "data/maps/RisingMeadow/scripts.inc"
+
+	.include "data/maps/RisingRock/scripts.inc"
